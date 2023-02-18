f22fda4376d80520724214ab66def9677e03e14e
github-sinkle <kirill.chanov@mail.ru>
2023-02-18 18:19:22 +0300

    some manipulation with model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,6 @@
 from odoo import models, fields
+from dateutil.relativedelta import relativedelta
+
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,16 +9,20 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=lambda self: fields.Date.today() + relativedelta(month=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, default='', copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
+    active = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
     )
-
+    state = fields.Selection(
+        required=True, default='new', copy=False,
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
+    )
